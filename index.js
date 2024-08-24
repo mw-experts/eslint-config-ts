7b6b895df4af842bb7f178e29dae44dd02352c42
Andrey Korovin <misticwonder@gmail.com>
2024-08-24 14:39:06 +0300

    update packages 5

--- index.js
+++ index.js
@@ -210,6 +210,7 @@ module.exports = {
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
+    "import/no-unresolved": "off",
     "import/order": [
       "error",
       {
